8a66bae3121f410bbd70b80cda3014a2460a3985
SamimSafi <samim.rahmany.sr@gmail.com>
2023-03-28 16:32:27 +0430

    New Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import { CreateContainer, Header, MainContainer } from "./components";
+import {
+  CreateContainer,
+  Header,
+  HomeContainer,
+  MainContainer,
+} from "./components";
 import { AnimatePresence } from "framer-motion";
 import { useStateValue } from "./context/StateProvider";
 import { getAllFoodItems } from "./utils/firebaseFunctions";
 import { actionType } from "./context/reducer";
+import { CardServices } from "./components/CardServices";
+import OurTeam from "./components/OurTeam";
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [{ foodItems }, dispatch] = useStateValue();
@@ -22,15 +32,21 @@ const App = () => {
   }, []);
   return (
     <AnimatePresence mode="wait">
-      <div className="w-screen h-auto flex flex-col bg-primary">
+      <div className="w-screen h-auto flex flex-col bg-primary scrollbar-none">
         <Header />
+
         {/* <main className="mt-14 md:mt-20 px-8 md:px-16 py-4 w-full"> */}
         <main className="w-full mt-14 md:mt-20 ">
+          <HomeContainer />
           <Routes>
             <Route path="/" element={<MainContainer />} />
-            <Route path="/createItem" element={<CreateContainer />} />
+            <Route path="/Services" element={<CardServices />} />
+            <Route path="/Team" element={<OurTeam />} />
+            <Route path="/About" element={<AboutUs />} />
+            <Route path="/Contact" element={<ContactUs />} />
           </Routes>
         </main>
+        <Footer />
       </div>
     </AnimatePresence>
   );
